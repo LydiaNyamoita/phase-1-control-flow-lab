91d080fa3654ddf4f6a08fd14a90100e8066f141
Lydia Nyamoita <lydianyamo18@outlook.com>
2023-06-28 22:18:10 +0300

    Done

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  if (someValue <= 400) {
+    return "This one is on me!";
+}
+else if (someValue > 2000 && someValue < 2500) {
+  return "I will gladly take your thirty bucks.";}
+
+  else {
+    return("No can do.")
+  }
+
+
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+
 }
 
 function switchOnCharmFromTip(){
